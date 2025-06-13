4239ebed21f8507e90527dee72c36997d70efe71
elkana <elkana911@yahoo.com>
2025-06-14 06:44:40 +0700

    redirect

--- gateway1/src/main/java/com/example/gateway1/config/SecurityConfig.java
+++ gateway1/src/main/java/com/example/gateway1/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -12,7 +13,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                return http.csrf(c -> c.disable())
+                return http.csrf(AbstractHttpConfigurer::disable)
+                                .cors(AbstractHttpConfigurer::disable)
                                 .authorizeHttpRequests(r -> r
                                                 .requestMatchers("/api/**").authenticated()
                                                 // .requestMatchers("/actuator/**").permitAll()
