11f88f19ab68a5e5f2b909145729c3bd4bf07cc5
elkana <elkana911@yahoo.com>
2025-06-15 09:24:01 +0700

    clientsecret

--- vaadin-sso1/src/main/java/com/example/vaadin_sso1/VaadinSso1Application.java
+++ vaadin-sso1/src/main/java/com/example/vaadin_sso1/VaadinSso1Application.java
@@ -2,9 +2,15 @@ package com.example.vaadin_sso1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.component.page.AppShellConfigurator;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@RestController
 @SpringBootApplication
 @Theme("my-theme")
 public class VaadinSso1Application implements AppShellConfigurator {
@@ -12,4 +18,11 @@ public class VaadinSso1Application implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(VaadinSso1Application.class, args);
     }
+
+    // to display access token
+    @GetMapping("/resource")
+    public String getResource(@RegisteredOAuth2AuthorizedClient("keycloak") OAuth2AuthorizedClient authorizedClient) {
+        return String.format("Resource info : %s", authorizedClient.getAccessToken().getTokenValue());
+    }
+
 }
